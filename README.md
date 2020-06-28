e56d51033315e136f49c83f6122b714c5af15903
ztyang <ztyang@mit.edu>
2020-06-28 17:29:14 -0400

    make tool

--- README.md
+++ README.md
@@ -1,2 +1,102 @@
 # ASIST-MC-toolbox
-Tools for extracting block types from Minecraft world folder and for generating videos of player trajectoriea
+Tools for extracting block types from Minecraft world folder and for generating videos of player trajectories
+
+## Required Python Libraries
+
+This tool depends on the following python libraries:
+
+```python
+Pillow, imageio ## for generating map, visualizing trajectory
+json, csv ## for writing and reading JDON and CSV formet
+cv2 ## for generating videos
+pprint ## for pretty print dictionary structure in debugging
+numpy ## for converting angle to radian
+tqdm ## for displaying a progress bar when running loops
+```
+
+---
+
+## Tool A - Extract map as PNG, CSV, and JSON
+
+We have a tool for extracting the 2D floor plan of structures built in Minecraft and for visualizing human trajectory data on the map as a mp4 file.
+
+This folder includes the following components:
+
+* `run_map_generator.ipynb` is the place to run the codes
+* `map_generator.py` defines the functions used by `run_map_generator.ipynb`
+* `outputs/` stores the outputmaps and traces
+* `resources/` stores the icons and distionaries used to generate the maps
+* `Singleplayer/` is the the Minecraft world folder to be extracted from
+* `MCWorldLib.egg` is a package we use for extracting region files ([source in Github](https://github.com/MestreLion/mcworldlib))
+
+The input to map_generator.py include two parts:
+
+* Minecraft world folder that's located in your Malmo folder `MalmoPlatform/Minecraft/run/saves/` (using Malmo) or `/Users/USER_NAME/Library/Application\ Support/minecraft/saves/` (using licenced Minecraft Java Version).
+```
+world = mc.load('Singleplayer')
+region = world.regions[-5,0]
+```
+
+Inside the `Singleplayer/region` folder, there are `.mca` files. Each `.mca` file specifies 32 x 32 chunks. Each chunk specifies 16 x 16 x 16 blocks. In the above example, we located an SAR building in `r.-5.0.mca`, which we found using online tool [Chunk viewer](https://pessimistress.github.io/minecraft/) (as shown in the image below). On the interface, adjust the bar on the left to the highest, click the minimap on the bottom right side, and navigate to the light-colored areas representing artificial structures. After we found the .mca file, we need to further narrow down the blocks.
+
+![](resources/imgs/tools-chunk.png)
+
+* the second part of the input is the ranges of x,y,z that specify the region of interests. This can be found by locating the building in the .mca file, then record down the coordinates of the top right and bottom left corner block on the ground (as the yellow/blue block shown in the image above). Then record down the lowest and highest level as y values.
+
+```
+x_low = -2176  # x of the bottom left block
+x_high = -2097  # x of the top right block
+z_low = 144     # z of the top right block
+z_high = 207  # z of the bottom left block
+y_low = 52
+y_high = 54
+```
+
+The outputs include three parts:
+
+* images of the 2D floor plans on different levels of the region of interest at `world-builder/outputs/*_map.png` where * = 0,1,2,9
+![](resources/imgs/tools-map.png)
+
+* a json file specifying the block type of all blocks within the region of interest at `world-builder/outputs/blocks_in_building.json`
+```
+# key = "x,y,z", value = block_type
+"-2152,52,160": "air",
+"-2151,52,160": "stained_hardened_clay",
+"-2150,52,160": "anvil", ...
+```
+* a csv file that represent the region of interest that can be used as input to our gridworld framework at `world-builder/outputs/darpa_maze.csv`
+![](resources/imgs/tools-csv.png)
+
+## Tool B - Visualize human trajectories as MP4
+
+Given DARPA testbed messages in the following format:
+
+```
+raw-data 1.58507E+12 {
+  app-id:"TestbedBusInterface",
+  mission-id:"6483fec4-153c-4994-8f42-a2e9b00d4db3",
+  routing-key:"testbed-message",
+  testbed-message:{
+    msg:{
+      trial_id:"6483fec4-153c-4994-8f42-a2e9b00d4db3",
+      sub_type:"state", source:"simulator",
+      timestamp:"2020-03-24T16:07:18.140779Z"
+    },
+    data:{
+      name:"Player396", entity_type:"human", life:20.0,
+      x:-2193.5, y:23.0, z:194.6328125,
+      pitch:0.0, yaw:0.0,
+      motion_x:0.0, motion_y:0.0, motion_z:0.11038550616085588
+    }
+  },
+  timestamp:1585066038141
+}
+```
+
+The codes visualize the location of the player on the map and infer the object that the player is interacting with.
+
+![](resources/imgs/tools-continuous.png)
+
+The codes further discretize the continuous position changes to generate a step by step trajectory that can be used as input to the inverse planning framework.
+
+![](resources/imgs/tools-discretized.png)
---
+++ map_generator.py
@@ -0,0 +1,291 @@
+import sys
+sys.path.append('MCWorldlib.egg')
+import mcworldlib as mc
+import math
+import json
+import os
+import pandas as pd
+import numpy as np
+from IPython.display import Image as iImage
+from PIL import Image
+from os.path import isfile
+
+def create_collage(width, height, cols, rows, name, listofimages):
+    """
+        take in a list of images and make a collage with the specified width and height
+    """
+
+    thumbnail_width = width//cols
+    thumbnail_height = height//rows
+    size = thumbnail_width, thumbnail_height
+    new_im = Image.new('RGB', (width, height))
+    ims = []
+    for p in listofimages:
+        im = Image.open(p)
+        im.thumbnail(size)
+        ims.append(im)
+    i = 0
+    x = 0
+    y = 0
+    for col in range(cols):
+        for row in range(rows):
+            if len(listofimages) == 16 * 16:
+                new_im.paste(ims[i], (width-x-int(width/cols), y))
+            else:
+                new_im.paste(ims[i], (x, y))
+            i += 1
+            y += thumbnail_height
+        x += thumbnail_width
+        y = 0
+
+    if len(listofimages) == 16 * 16:
+        new_im = new_im.rotate(90)
+
+    new_im.save(name)
+
+# first version of the Singleplayer world
+# all_blocks = generate_maps(region, (23, 26, 9, 11, 12, 14, 1))
+def generate_maps(world, region, ranges):
+
+    x_low, x_high, z_low, z_high, y_low, y_high = ranges
+    x_ind_low = int(x_low / 16 - region[0] * 32)  # int((2560 + x_low)/16)
+    x_ind_high = int(x_high / 16 - region[0] * 32)
+    z_ind_low = int(z_low / 16 - region[1] * 32)
+    z_ind_high = int(z_high / 16 - region[1] * 32)
+    y_ind = math.floor(y_low / 16)
+    y_ind_low = int(y_low - y_ind * 16)
+    y_ind_high = int(y_high - y_ind * 16)
+    print(x_ind_low, x_ind_high, z_ind_low, z_ind_high, y_ind_low, y_ind_high)
+    region_blocks = world.regions[region[0], region[1]]
+
+    # ---------------------------------------------------------
+    # get the mapping from block id to block type
+    block_dict = {}
+    with open('resources/block_id.json') as json_file:
+        entries = json.load(json_file)
+        for entry in entries:
+            if entry['type'] not in block_dict:
+                block_dict[entry['type']] = entry['text_type']
+
+    with open("resources/block_to_texture.json") as json_file:
+        images = json.load(json_file)
+
+    # ---------------------------------------------------------
+    ## read all blocks in region and store in all_blocks, where 'x' 'y' 'z' specifies patch index
+    ## e.g., all_blocks[(-2176, 52, 144)] = {'block_type': 'air', 'y': 4, 'z': 0, 'x': 0}
+    all_blocks = {}
+    cannot_find_blocks = []
+    collage_layers = {}
+
+    for x_ind in range(x_ind_low, x_ind_high+1):
+        for z_ind in range(z_ind_low, z_ind_high+1):
+
+            # only look at the floor level
+            blocks = region_blocks[x_ind, z_ind]['']['Level']['Sections'][y_ind]['Blocks']
+            blocks = list(blocks)
+
+            # offsets in the game world
+            x_0 = 16* (x_ind + region[0]*32)
+            y_0 = 16*y_ind
+            z_0 = 16* (z_ind + region[1]*32) #+ 15
+
+            # store all blocks in an array to be printed on a 16 by 16 image
+            image_layer = []
+            for i in range(len(blocks)):
+
+                # to be stored about each block
+                stats = {}
+
+                # 'block_type'
+                num = int(blocks[i])
+                if num < 0:
+                    num = 256+num
+                stats['block_type'] = block_dict[num]
+
+                # coordinates 'x' 'y' 'z'
+                stats['y'] = math.floor(i/16/16)
+                i = i % (16*16)
+                stats['z'] = math.floor(i/16)
+                stats['x'] = i % 16
+
+                # collect blocks and print images
+                if stats['y'] >= y_ind_low and stats['y'] <= y_ind_high:
+                    all_blocks[(stats['x'] + x_0, stats['y'] + y_0, stats['z'] + z_0)] = stats
+
+                    # generate image pathes of the region
+                    if stats['block_type'] in images.keys():
+                        texture = 'resources/myblocks/'+images[stats['block_type']]
+                    else:
+                        texture = 'resources/myblocks/nether_brick.png'
+                        cannot_find_blocks.append(stats['block_type'])
+
+                    image_layer.append(texture)
+                    if stats['x'] == 15 and stats['z'] == 15:
+                        floor_level = stats['y']-y_ind_low
+                        name = '[' + str(x_ind) + ',' + str(z_ind)
+                        name += ',' + str(floor_level) +']'
+                        name = 'outputs/floors/' + name + "_floor.jpg"
+                        create_collage(16*16, 16*16, 16, 16, name, image_layer)
+                        if floor_level not in collage_layers:
+                            collage_layers[floor_level] = []
+                        collage_layers[floor_level].append(name)
+                        image_layer = []
+
+    for block in set(cannot_find_blocks):
+        print('cannot find image in resources/myblocks/',block)
+
+    # ---------------------------------------------------------
+    ## We merge multiple floor plans to prioritize showing the important objects.
+    ## This way we will get one floor plan as the base for visualizing human trajectories
+    important_blocks = {}
+
+    ## create the dict for one block each (x,z) location
+    for key in all_blocks.keys():
+        x,y,z = key
+
+        block_type = all_blocks[key]['block_type']
+        WRITTEN = False
+        if y == y_low+2 and (block_type == 'wall_sign'):
+            important_blocks[x,z] = all_blocks[(x,y_low+2,z)]
+            WRITTEN = True
+        elif y == y_low+1 and (block_type == 'lever' or block_type == 'wall_sign'):
+            important_blocks[x,z] = all_blocks[(x,y_low+1,z)]
+            WRITTEN = True
+        elif y == y_low:
+            if block_type == 'stained_hardened_clay' and all_blocks[(x,y_low+2,z)]['block_type'] == 'air':
+                important_blocks[x,z] = all_blocks[(x,y_low+2,z)]
+                WRITTEN = True
+            elif block_type == 'stained_hardened_clay' and all_blocks[(x,y_low+1,z)]['block_type'] == 'air':
+                important_blocks[x,z] = all_blocks[(x,y_low+1,z)]
+                WRITTEN = True
+            else:
+                important_blocks[x,z] = all_blocks[(x,y_low,z)]
+                WRITTEN = True
+
+        if WRITTEN:
+            if all_blocks[(x,y_low+1,z)]['block_type'] == 'air':
+                important_blocks[x,z]['open_top'] = True
+            else:
+                important_blocks[x,z]['open_top'] = False
+
+    ## generate collage
+    for key in important_blocks.keys():
+        x,z = key
+        stats = important_blocks[key]
+        image_layer.append('resources/myblocks/'+images[stats['block_type']])
+
+        if stats['x'] == 15 and stats['z'] == 15:
+            floor_level = 9
+            name = '[' + str(round(x/16 - region[0] * 32)) + ',' + str(round(z/16 - region[1] * 32))
+            name += ',' + str(floor_level) +']'
+            name = 'outputs/floors/' + name + "_floor.jpg"
+            create_collage(16*16, 16*16, 16, 16, name, image_layer)
+            if floor_level not in collage_layers:
+                collage_layers[floor_level] = []
+            collage_layers[floor_level].append(name)
+            image_layer = []
+
+    # ---------------------------------------------------------
+    ## make collage out of smaller patches
+    x_len = x_ind_high - x_ind_low + 1
+    z_len = z_ind_high - z_ind_low + 1
+    for key in collage_layers.keys():
+        name = 'outputs/'+str(key) + '_map.png'
+        create_collage(16*16*x_len, 16*16*z_len, x_len, z_len, name, collage_layers[key])
+        for image in collage_layers[key]:
+            os.remove(image)
+        if key == 9:  display(iImage(filename=name))
+
+    return all_blocks, important_blocks
+
+def generate_json(all_blocks, ranges):
+
+    blocks_in_building = {}
+
+    ## includes the types of all blocks
+    blocks_to_json = {}
+    for key,value in all_blocks.items():
+        key = str(key).replace('(','').replace(')','').replace(' ','')
+        blocks_to_json[key] = value['block_type']
+
+    blocks_in_building['blocks'] = blocks_to_json
+
+    ## includes the range limites of the region
+    region = {}
+    region['x_low'] = ranges[0]
+    region['x_high'] = ranges[1]
+    region['z_low'] = ranges[2]
+    region['z_high'] = ranges[3]
+    region['y_low'] = ranges[4]
+    region['y_high'] = ranges[5]
+    blocks_in_building['region'] = region
+
+    with open('outputs/blocks_in_building.json', 'w') as outfile:
+        json.dump(blocks_in_building, outfile)
+
+def generate_csv(important_blocks, indices):
+
+    x_low, x_high, z_low, z_high, y_low, y_high = indices
+
+    count = 0
+    objects = []
+    objects2grids = {
+        'wool':'V',
+        'prismarine':'V',
+        'gold_block':'VV',
+        'wooden_door':'D',
+        'dark_oak_door':'D',
+        'gravel':'G',
+        'fire':'F',
+        'lever':'',
+        'wall_sign':'',
+        'standing_sign':''
+    #     'air': '',
+    #     'end_portal_frame':'',
+    #     'cauldron':'',
+    #     'dispenser':'',
+    #     '':'',
+    #     'stained_hardened_clay': 'W',
+    #     'anvil': 'W',
+    #     'hopper': 'W',
+    #     'cobblestone_wall':'W',
+    #     'monster_egg':'W',
+    #     'cobblestone':'W',
+    #     'quartz_stairs':'W',
+    #     'clay':'W',
+    #     '':'W',
+    }
+    data = pd.DataFrame('', index=np.arange(z_high - z_low + 1), columns=np.arange(x_high - x_low + 1))
+    for key in important_blocks.keys():
+        count += 1
+        block = important_blocks[key]
+        x = key[0] - x_low
+        z = key[1] - z_low
+        block_type = block['block_type']
+        if block_type in objects2grids.keys():
+            csv_block = objects2grids[block_type]
+        else:
+            if block['open_top']:
+                csv_block = ''
+            else:
+                csv_block = 'W'
+        data[x][z] = csv_block
+    #     print(count, x, z, block['block_type'], csv_block)
+    #     if block['block_type'] not in objects:
+    #         objects.append(block['block_type'])
+    # print(objects)
+    data.to_csv('outputs/darpa_maze.csv', index=False, header=False)
+
+# ---------------------------------------------------------
+## for comparing changes between another json file
+# with open('blocks_in_building.json') as json_file:
+#     data = json.load(json_file)
+#     count = 0
+#     count2 = 0
+#     for key,value in data.items():
+#         if key in blocks_to_json.keys():
+#             count += 1
+#             if value != blocks_to_json[key]:
+#                 count2 += 1
+#                 print(key, blocks_to_json[key], value)
+#     print(count, count2)
